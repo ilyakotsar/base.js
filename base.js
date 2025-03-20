3ba11ad2175efeeafacba19f3b36eda1aff1d0ba
Ilya Kotsar <ilyakotsar@yahoo.com>
2025-03-20 13:02:10 +0700

    Update

--- base.js
+++ base.js
@@ -1,10 +1,30 @@
-if (!displayNoneClass) {
-    var displayNoneClass = 'hidden';
+if (!baseJsPrefix) {
+    var baseJsPrefix = 'b-';
 }
-if (!csrfTokenName) {
-    var csrfTokenName = 'csrfmiddlewaretoken';
+if (!baseJsDisplayNoneClass) {
+    var baseJsDisplayNoneClass = 'hidden';
+}
+if (!baseJsCsrfTokenName) {
+    var baseJsCsrfTokenName = 'csrfmiddlewaretoken';
 }
 
+function baseJsAddStyles() {
+    let style = document.createElement('style');
+    style.innerText = `.${baseJsDisplayNoneClass} {display: none;} `;
+    style.innerText += 'button span, button svg, button i, button img {pointer-events: none;}';
+    document.head.appendChild(style);
+}
+
+baseJsAddStyles();
+
+/**
+ * 
+ * @param {string} method 
+ * @param {string} url 
+ * @param {object} data (optional) 
+ * @param {string} indicator (optional) 
+ * @returns {Promise<string>|Promise<any>}
+ */
 async function baseJsRequest(method, url, data, indicator) {
     let options = {
         method: method.toUpperCase(),
@@ -18,17 +38,17 @@ async function baseJsRequest(method, url, data, indicator) {
         options.headers = {
             'Content-Type': 'application/json',
         }
-        let csrfTokenElement = document.querySelector(`[name=${csrfTokenName}]`);
+        let csrfTokenElement = document.querySelector(`[name=${baseJsCsrfTokenName}]`);
         if (csrfTokenElement) {
             options.headers['X-CSRFToken'] = csrfTokenElement.value;
         }
     }
     if (indicator) {
-        document.getElementById(indicator).classList.remove(displayNoneClass);
+        document.getElementById(indicator).classList.remove(baseJsDisplayNoneClass);
     }
     let response = await fetch(url, options);
     if (indicator) {
-        document.getElementById(indicator).classList.add(displayNoneClass);
+        document.getElementById(indicator).classList.add(baseJsDisplayNoneClass);
     }
     let contentType = response.headers.get('Content-Type');
     if (contentType && contentType.includes('application/json')) {
@@ -38,132 +58,147 @@ async function baseJsRequest(method, url, data, indicator) {
     }
 }
 
-function baseJsAfterRequest(response, key, target, swap) {
-    let keyArray = key.split(',');
+/**
+ * 
+ * @param {object} element 
+ * @param {string} target 
+ * @param {string|number} value 
+ * @param {string} swap 
+ */
+function baseJsSwap(element, target, value, swap) {
+    let elements = [];
+    if (target.slice(0, 5) === 'name=') {
+        let name = target.split('name=')[1];
+        elements = document.querySelectorAll(`[name="${name}"]`);
+    } else if (target === 'this') {
+        elements.push(element);
+    } else {
+        elements = document.querySelectorAll(target);
+    }
+    elements.forEach(element => {
+        switch (swap) {
+            case 'inner':
+                element.innerHTML = value;
+                break;
+            case 'outer':
+                element.outerHTML = value;
+                break;
+            case 'value':
+                element.value = value;
+                break;
+            case 'class':
+                element.className = value;
+                break;
+            case 'addClass':
+                element.classList.add(value);
+                break;
+            case 'removeClass':
+                element.classList.remove(value);
+                break;
+            case 'toggleClass':
+                element.classList.toggle(value);
+                break;
+        }
+    });
+}
+
+/**
+ * 
+ * @param {object} element
+ * @param {string|object} response 
+ * @param {string} target 
+ * @param {string} swap 
+ * @param {string} key (optional) 
+ */
+function baseJsAfterRequest(element, response, target, swap, key) {
     let targetArray = target.split(',');
     let swapArray = swap.split(',');
-    for (let i = 0; i < keyArray.length; i++) {
-        let key = keyArray[i].trim();
+    let keyArray = key.split(',');
+    for (let i = 0; i < targetArray.length; i++) {
         let target = targetArray[i].trim();
         let swap = swapArray[i].trim();
+        let key = keyArray[i].trim();
         let value = response;
         if (typeof response === 'object') {
-            let k = key.split('.');
-            for (let i = 0; i < k.length; i++) {
-                value = value[k[i]];
-            }
+            key.split('.').forEach(part => {
+                value = value[part];
+            });
         }
-        let elements;
-        if (target.slice(0, 5) === 'name=') {
-            let name = target.split('name=')[1];
-            elements = document.querySelectorAll(`[name="${name}"]`);
-        } else {
-            elements = document.querySelectorAll(target);
-        }
-        for (let x = 0; x < elements.length; x++) {
-            let element = elements[x];
-            switch (swap) {
-                case 'inner':
-                    element.innerHTML = value;
-                    break;
-                case 'outer':
-                    element.outerHTML = value;
-                    break;
-                case 'value':
-                    element.value = value;
-                    break;
-                case 'class':
-                    element.className = value;
-                    break;
-                case 'addClass':
-                    element.classList.add(value);
-                    break;
-                case 'removeClass':
-                    element.classList.remove(value);
-                    break;
-                case 'toggleClass':
-                    element.classList.toggle(value);
-                    break;
-            }
-        }
+        baseJsSwap(element, target, value, swap);
     }
 }
-
-function baseJsDisplay(button, targetId, iconShowClass, iconHideClass, iconLocationId) {
-    let target = document.getElementById(targetId);
-    let iconElement;
-    if (iconLocationId) {
-        iconElement = document.getElementById(iconLocationId);
-    } else {
-        iconElement = button.querySelector('i');
-    }
-    if (target.classList.contains(displayNoneClass)) {
-        target.classList.remove(displayNoneClass);
-        if (iconElement) {
-            iconElement.classList.replace(iconShowClass, iconHideClass);
-        }
-    } else {
-        target.classList.add(displayNoneClass);
-        if (iconElement) {
-            iconElement.classList.replace(iconHideClass, iconShowClass);
-        }
-    }
-}
-
-function baseJsAddStyles() {
-    let style = document.createElement('style');
-    style.innerText = `.${displayNoneClass} {display: none;} `;
-    style.innerText += 'button span, button svg, button i, button img {pointer-events: none;}';
-    document.head.appendChild(style);
-}
-
-baseJsAddStyles();
 
 window.addEventListener('click', function(event) {
-    let button = event.target;
-    if (button.getAttribute('b-get')) {
-        let url = button.getAttribute('b-get');
-        let target = button.getAttribute('b-target');
-        let key = button.getAttribute('b-key');
-        let swap = button.getAttribute('b-swap');
-        let indicator = button.getAttribute('b-indicator');
-        baseJsRequest('get', url, {}, indicator).then(response => {
-            baseJsAfterRequest(response, key, target, swap);
+    let element = event.target;
+    let getUrl = element.getAttribute(`${baseJsPrefix}get`);
+    let postUrl = element.getAttribute(`${baseJsPrefix}post`);
+    let apply = element.getAttribute(`${baseJsPrefix}apply`);
+    let display = element.getAttribute(`${baseJsPrefix}display`);
+    let target, key, swap, indicator;
+    if (getUrl || postUrl || apply) {
+        target = element.getAttribute(`${baseJsPrefix}target`);
+        swap = element.getAttribute(`${baseJsPrefix}swap`);
+    }
+    if (getUrl || postUrl) {
+        key = element.getAttribute(`${baseJsPrefix}key`);
+        indicator = element.getAttribute(`${baseJsPrefix}indicator`);
+    }
+    if (getUrl) {
+        baseJsRequest('get', getUrl, {}, indicator).then(response => {
+            baseJsAfterRequest(element, response, target, swap, key);
         });
-    } else if (button.getAttribute('b-post')) {
-        let url = button.getAttribute('b-post');
-        let target = button.getAttribute('b-target');
-        let key = button.getAttribute('b-key');
-        let swap = button.getAttribute('b-swap');
-        let indicator = button.getAttribute('b-indicator');
-        let form = button.getAttribute('b-form');
-        let data = {};
-        let children = button.children;
-        for (let i = 0; i < children.length; i++) {
-            let name = children[i].name;
-            let value = children[i].value;
-            if (name && value) {
-                data[name] = value;
-            }
-        }
+    } else if (postUrl) {
+        let form = element.getAttribute(`${baseJsPrefix}form`);
+        let formElements = [element.children];
         if (form) {
-            let children = document.getElementById(form).children;
-            for (let i = 0; i < children.length; i++) {
-                let name = children[i].name;
-                let value = children[i].value;
+            let children = this.document.getElementById(form).children;
+            formElements.push(children);
+        }
+        let data = {};
+        formElements.forEach(element => {
+            for (let i = 0; i < element.length; i++) {
+                let name = element[i].name;
+                let value = element[i].value;
                 if (name && value) {
                     data[name] = value;
                 }
             }
-        }
-        baseJsRequest('post', url, data, indicator).then(response => {
+        });
+        baseJsRequest('post', postUrl, data, indicator).then(response => {
             baseJsAfterRequest(response, key, target, swap);
         });
-    } else if (button.getAttribute('b-display')) {
-        let target = button.getAttribute('b-display');
-        let iconShow = button.getAttribute('b-icon-show');
-        let iconHide = button.getAttribute('b-icon-hide');
-        let iconLocation = button.getAttribute('b-icon-location');
-        baseJsDisplay(button, target, iconShow, iconHide, iconLocation);
+    } else if (apply) {
+        let value = apply;
+        let valueArray = value.split(',');
+        let targetArray = target.split(',');
+        let swapArray = swap.split(',');
+        for (let i = 0; i < targetArray.length; i++) {
+            let value = valueArray[i].trim();
+            let target = targetArray[i].trim();
+            let swap = swapArray[i].trim();
+            baseJsSwap(element, target, value, swap);
+        }
+    } else if (display) {
+        let iconShow = element.getAttribute(`${baseJsPrefix}icon-show`);
+        let iconHide = element.getAttribute(`${baseJsPrefix}icon-hide`);
+        let iconLocation = element.getAttribute(`${baseJsPrefix}icon-location`);
+        let target = document.getElementById(display);
+        let iconElement;
+        if (iconLocation) {
+            iconElement = document.getElementById(iconLocation);
+        } else {
+            iconElement = element.querySelector('i');
+        }
+        if (target.classList.contains(baseJsDisplayNoneClass)) {
+            target.classList.remove(baseJsDisplayNoneClass);
+            if (iconElement) {
+                iconElement.classList.replace(iconShow, iconHide);
+            }
+        } else {
+            target.classList.add(baseJsDisplayNoneClass);
+            if (iconElement) {
+                iconElement.classList.replace(iconHide, iconShow);
+            }
+        }
     }
 });
